b93df525221bb756af508218cbeed15284629463
CBhushan <cbhushan@users.noreply.github.com>
2017-02-19 15:00:51 -0500

    added support for .scm files

--- GIMP-plugins/deploy-gimp-plugins.sh
+++ GIMP-plugins/deploy-gimp-plugins.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 # Installs GIMP plugins to user's gimp plugin directory
 
-
 srcDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-targetDir="$HOME"/".gimp-2.8/plug-ins"
+targetDir="$HOME"/".gimp-2.8"
 
 if [ -d "$targetDir" ]; then
-    cp -p -f "$srcDir"/*.py "$targetDir"/
+    echo "Copying scripts to $targetDir"
+    
+    pluginDir="$targetDir"/"plug-ins"
+    mkdir -p "$pluginDir"
+    cp -p -f "$srcDir"/*.py "$pluginDir"/
+
+    scriptDir="$targetDir"/"scripts"
+    mkdir -p "$scriptDir"
+    cp -p -f "$srcDir"/*.scm "$scriptDir"/
 else
     echo "Gimp directory not found: $targetDir"
+    exit 1
 fi
 
