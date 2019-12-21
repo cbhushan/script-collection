47aeefc29970d6ad0e20760ded86f5baf83077e7
CBhushan <cbhushan@users.noreply.github.com>
2019-12-21 11:14:02 -0500

    updated video concat script

--- video/concat-videos-with-same-encoding.sh
+++ video/concat-videos-with-same-encoding.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
-# This script concatenates videos with identical codecs, like those recoded by same video cameras (eg: dashbaord, same phone etc.).
-# It uses ffmpeg with demuxer method as described here: http://trac.ffmpeg.org/wiki/Concatenate#demuxer
-#
-# Codes used across different video files must be identical to generate reasonable output!
-#
-# Copyright C Bhushan; Licensed under the Apache License v2.0.
-# https://github.com/cbhushan/script-collection
-#
+read -r -d '' USAGE << EOM
+This script concatenates videos with identical codecs or concatenable container.
+Use -demux option for video with same codec, like those recoded by same video 
+camera, eg: dashbaord, same phone etc.
+Use -cat for concatenable containers, which allows file-level concatenation of 
+files, eg: MPEG-1, MPEG-2 PS, DV etc.
+
+Usage:
+   $0 </path/to/video-directory> </path/to/output-file.ext> <-demux or -cat>  [<ext>]
+   $0 </path/to/video-list.txt>  </path/to/output-file.ext> -demux  [<ext>]
+
+It uses ffmpeg with concat demuxer or concat protocol as described here:
+http://trac.ffmpeg.org/wiki/Concatenate
+http://ffmpeg.org/faq.html#How-can-I-concatenate-video-files
+
+Copyright C Bhushan; Licensed under the Apache License v2.0.
+https://github.com/cbhushan/script-collection
+EOM
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 . "$script_dir"/../bash-functions.sh
@@ -14,14 +24,13 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 assert_program_is_available ffmpeg
 FFMPEG="$(command -v ffmpeg)"
 
-if [ "$#" -lt 2 ] || [ "$#" -gt 3 ] ; then
-    echo "Incorrect usage: Illegal number of parameters!"
-    echo -e "\nThis script concatenates videos with identical encoding, like those recoded by dashcam video cameras."
-    echo -e "Usage:\n\t $0 </path/to/video-directory OR /path/to/video-list.txt>  </path/to/output-file.ext>  [<ext>]"
+if [ "$#" -lt 3 ] || [ "$#" -gt 4 ] ; then
+    echo -e "Incorrect usage: Illegal number of parameters!\n"
+    echo "${USAGE}"
     exit 1
 
-elif [ "$#" -eq 3 ] ; then
-    EXT="$3"
+elif [ "$#" -eq 4 ] ; then
+    EXT="$4"
 
 else
     filename="$(basename -- "${2}")"
@@ -29,10 +38,28 @@ else
     echo "No extesion specified. Using extension from output-file: ${EXT}"
 fi
 
+case "$3" in
+
+  -demux)
+    use_demux=true
+    ;;
+
+  -cat)
+    use_demux=false
+    ;;
+
+  *)
+    echo -n "ERROR: Unknown flag for third argument: ${3}"
+    echo "${USAGE}"
+    exit 1
+    ;;
+esac
+
+
 mkdir -p "$(dirname "${2}")"
 out_file="$(realpath "${2}")"
 if [ -f "$out_file" ] ; then
-    echo "Error: Output file already exists: $out_file"
+    echo "ERROR: Output file already exists: $out_file"
     echo -e "\t Please remove output file to continue with merging! Exiting..."
     exit 1
 fi
@@ -58,15 +85,24 @@ if [ -d "$1" ] ; then
     fi 
 
     # create video-list file
-    tmp_list=$(mktemp)
-    for f in "${srcdir}"/*.${EXT}; do
-        echo "file '$f'" >> "$tmp_list"
-    done
-    list_file="$tmp_list"
+    if [ "$use_demux" = true ] ; then
+        tmp_list=$(mktemp)
+        for f in "${srcdir}"/*.${EXT}; do
+            echo "file '$f'" >> "$tmp_list"
+        done
+        list_file="$tmp_list"
+    
+    else
+        file_list=$(ls "${srcdir}"/*.${EXT} | xargs printf '%s|')
+        
+    fi
     
 elif [ -f "$1" ] ; then
     list_file="$1"
-    
+    if [ "$use_demux" = false ] ; then
+        echo "Error: video-list.txt can only be used with -demux flag."
+        exit 1
+    fi
 else
     echo "Error: Directory/File not found or does not exists: $1"
     exit 1
